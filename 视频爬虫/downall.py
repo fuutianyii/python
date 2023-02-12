614492e90a22b29db0de01637e29551265f00953
fuutianyii <fuutianyii@gmail.com>
2023-02-12 23:35:56 +0800

    main

--- 视频爬虫/downall.py
+++
@@ -1,8 +0,0 @@
-import os
-f=open("m3u8.txt","r")
-m3u8line=f.readlines()
-num=18
-for i in m3u8line:
-    m3u8=i.replace("\n","")
-    os.system("python f:/python/视频爬虫/optdown.py -u "+m3u8+" -n "+str(num)+".mp4")
-    num+=1
